38805bc73edcb24cf58b0a2def5a5ff132df1f86
Aloun Vangkeosay <alounv@gmail.com>
2026-01-18 03:11:21 +0100

    08-2

--- src/day08.zig
+++ src/day08.zig
@@ -20,9 +20,36 @@ fn distance3D(p1: Point, p2: Point) u64 {
     return dx * dx + dy * dy + dz * dz;
 }
 
+fn listPoints(points: []Point, input: []const u8) !void {
+    var lines = std.mem.splitScalar(u8, input, '\n');
+    var l: usize = 0;
+    while (lines.next()) |line| : (l += 1) {
+        if (line.len == 0) continue;
+        var words = std.mem.splitScalar(u8, line, ',');
+        points[l] = .{
+            try std.fmt.parseUnsigned(u64, words.next().?, 10),
+            try std.fmt.parseUnsigned(u64, words.next().?, 10),
+            try std.fmt.parseUnsigned(u64, words.next().?, 10),
+        };
+    }
+}
+
+fn getPairsSortedByDistance(pairs: []Pair, points: []const Point) usize {
+    var index: usize = 0;
+    for (points, 0..) |p1, i| {
+        for (points[i + 1 ..], (i + 1)..) |p2, j| {
+            pairs[index] = .{ .p1 = i, .p2 = j, .dist = distance3D(p1, p2) };
+            index += 1;
+        }
+    }
+    std.mem.sort(Pair, pairs[0..index], {}, Pair.lessThan);
+    return index;
+}
+
 fn UnionFind(comptime max: usize) type {
     return struct {
         parent: [max]usize,
+        groupCount: usize,
 
         const Self = @This();
 
@@ -31,7 +58,7 @@ fn UnionFind(comptime max: usize) type {
             for (0..max) |i| {
                 parent[i] = i;
             }
-            return .{ .parent = parent };
+            return .{ .parent = parent, .groupCount = max };
         }
 
         /// Find the root of the set containing x with path compression.
@@ -52,7 +79,12 @@ fn UnionFind(comptime max: usize) type {
             const py = self.find(y);
             if (px != py) {
                 self.parent[py] = px;
+                self.groupCount -= 1;
             }
+        }
+
+        fn hasOnlyOneGroup(self: *Self) bool {
+            return self.groupCount == 1;
         }
     };
 }
@@ -64,29 +96,11 @@ fn solveP1(
 ) !u64 {
     // List points
     var points: [max]Point = undefined;
-    var lines = std.mem.splitScalar(u8, input, '\n');
-    var l: usize = 0;
-    while (lines.next()) |line| : (l += 1) {
-        if (line.len == 0) continue;
-        var words = std.mem.splitScalar(u8, line, ',');
-        points[l] = .{
-            try std.fmt.parseUnsigned(u64, words.next().?, 10),
-            try std.fmt.parseUnsigned(u64, words.next().?, 10),
-            try std.fmt.parseUnsigned(u64, words.next().?, 10),
-        };
-    }
+    try listPoints(&points, input);
 
     // List pairs and distances and sort by distances
-    var index: usize = 0;
-    const pairsCount = max * (max - 1) / 2;
-    var pairs: [pairsCount]Pair = undefined;
-    for (points, 0..) |p1, i| {
-        for (points[i + 1 ..], (i + 1)..) |p2, j| {
-            pairs[index] = .{ .p1 = i, .p2 = j, .dist = distance3D(p1, p2) };
-            index += 1;
-        }
-    }
-    std.mem.sort(Pair, pairs[0..index], {}, Pair.lessThan);
+    var pairs: [max * (max - 1) / 2]Pair = undefined;
+    _ = getPairsSortedByDistance(&pairs, &points);
 
     // Union-Find to group points
     var uf = UnionFind(max).init();
@@ -107,14 +121,26 @@ fn solveP1(
     return groupSizes[0] * groupSizes[1] * groupSizes[2];
 }
 
-fn solveP2(comptime max: usize, input: []const u8) u64 {
-    _ = max;
-    _ = input;
-    const part2: u64 = 0;
+fn solveP2(comptime max: usize, input: []const u8) !u64 {
+    // List points
+    var points: [max]Point = undefined;
+    try listPoints(&points, input);
 
-    // TODO: Implement solution
+    // List pairs and distances and sort by distances
+    var pairs: [max * (max - 1) / 2]Pair = undefined;
+    const pairCount = getPairsSortedByDistance(&pairs, &points);
 
-    return part2;
+    // Union-Find to group points
+    var uf = UnionFind(max).init();
+    for (pairs[0..pairCount]) |pair| {
+        uf.unite(pair.p1, pair.p2);
+
+        if (uf.hasOnlyOneGroup()) {
+            return points[pair.p1][0] * points[pair.p2][0];
+        }
+    }
+
+    return 0;
 }
 
 // ============================================================================
@@ -122,7 +148,7 @@ fn solveP2(comptime max: usize, input: []const u8) u64 {
 // ============================================================================
 
 const EXPECTED_PART1: i64 = 181584;
-const EXPECTED_PART2: i64 = 0;
+const EXPECTED_PART2: i64 = 8465902405;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -133,7 +159,7 @@ pub fn main() !void {
     defer allocator.free(content);
 
     const part1 = try solveP1(1000, 1000, content);
-    const part2 = solveP2(143, content);
+    const part2 = try solveP2(1000, content);
 
     // Verify expected results
     if (part1 != EXPECTED_PART1) {
@@ -202,6 +228,6 @@ test "part2 example" {
         \\425,690,689
     ;
 
-    const result = solveP2(20, example_input);
-    try std.testing.expectEqual(@as(u64, 0), result);
+    const result = try solveP2(20, example_input);
+    try std.testing.expectEqual(@as(u64, 25272), result);
 }
